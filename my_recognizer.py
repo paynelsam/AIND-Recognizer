8ffef4a49d05fad6f038f0cfe0677d056e7e873c
paynelsam <paynelsam@gmail.com>
2017-05-27 16:04:02 -0700

    solution: add model selector
    Signed-off-by: paynelsam <paynelsam@gmail.com>

--- my_recognizer.py
+++ my_recognizer.py
@@ -1,6 +1,7 @@
 import warnings
 from asl_data import SinglesData
 
+ERROR_LOGGING = False
 
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
@@ -22,4 +23,53 @@ def recognize(models: dict, test_set: SinglesData):
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
+
+    # get the list of test sequences
+    test_sequences = list(test_set.get_all_Xlengths().values())
+    for test_X, test_Xlength in test_sequences:
+        # create a dictionary of (word, prob) for this test case
+        probabilities.append({})
+        guesses.append("")
+        # find the score for each model (each model has a word it is trained for)
+        for word, model in models.items():
+            try:
+                score = model.score(test_X, test_Xlength)
+                # store set of probabilities, we'll find the best one later
+                probabilities[-1][word] = score
+            except Exception as e:
+                if ERROR_LOGGING:
+                    print(str(e))
+                # if a probability cannot be calculated, set to -inf instead
+                probabilities[-1][word] = float("-infinity")
+                continue;
+        # find the best guess
+        guesses[-1] = max(probabilities[-1], key=probabilities[-1].get)
+
+    return (probabilities, guesses)
+
+    """
+    for word_id in range (0, len(test_set.get_all_Xlengths())):
+        X, lengths = test_set.get_word_Xlengths[word_id]
+        current_sequence = test_set.get_item_sequences(word_id)
+        print (current_sequence)
+        current_length = test_set.get_item_Xlengths(word_id)
+        for word, model in models.items():
+            score = model.score(current_sequence, current_length)
+            if (probabilities[word_id] < p):
+                probabilities[word_id] = p
+                guesses[word_id] = word
     raise NotImplementedError
+    """
+
+    """
+    for word, model in models.items():
+        word_id = test_set.wordlist.index(word)
+        print (word)
+        print (word_id)
+        print(model.score(test_set.get_item_Xlengths(word_id)))
+        if (probabilities[word_id] < p):
+            probabilities[word_id] = p
+            guesses[word_id] = word
+    """
+    raise NotImplementedError
+    return (probabilities, guesses)
